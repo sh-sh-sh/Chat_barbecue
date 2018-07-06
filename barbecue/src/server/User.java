d2b23d4a635d9070266a8006080dc8a5bedda6e7
Seo Hee <sh265601@gmail.com>
2018-07-06 19:39:44 +0900

    transcode to UTF-8

--- barbecue/src/server/User.java
+++ barbecue/src/server/User.java
@@ -5,7 +5,7 @@ import java.io.BufferedOutputStream;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
-import java.net.Socket;
+import java.net.Socket; 
 
 import client.FileSender;
 
